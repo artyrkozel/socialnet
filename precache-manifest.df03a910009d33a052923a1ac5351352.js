989fec6ea88354db693bf59d010e43d9114e6460
artyrkozel <artyr_42@mail.ru>
2021-02-12 14:36:38 +0300

    Updates

---
+++ precache-manifest.df03a910009d33a052923a1ac5351352.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c98329c35398f5dd022a0ed7abaac2",
+    "url": "/artyrkozel/socialnet/index.html"
+  },
+  {
+    "revision": "92db14764f4f20a4598d",
+    "url": "/artyrkozel/socialnet/static/css/main.90268696.chunk.css"
+  },
+  {
+    "revision": "dccc2512093e7f11c414",
+    "url": "/artyrkozel/socialnet/static/js/2.13a6564b.chunk.js"
+  },
+  {
+    "revision": "4188741b4aa7f08c502a78e46dabcf11",
+    "url": "/artyrkozel/socialnet/static/js/2.13a6564b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92db14764f4f20a4598d",
+    "url": "/artyrkozel/socialnet/static/js/main.2953ab4c.chunk.js"
+  },
+  {
+    "revision": "c96c69efa0d28c34cb70",
+    "url": "/artyrkozel/socialnet/static/js/runtime-main.97d20dea.js"
+  },
+  {
+    "revision": "bddb2c31b933f7ad7ac9e1fdcb180620",
+    "url": "/artyrkozel/socialnet/static/media/loader.bddb2c31.gif"
+  },
+  {
+    "revision": "0f30a30bafe9b8b965b641b5a0c9803d",
+    "url": "/artyrkozel/socialnet/static/media/logog.0f30a30b.png"
+  },
+  {
+    "revision": "eee2f3229ca6e0d9f9092f4a1f929b55",
+    "url": "/artyrkozel/socialnet/static/media/newsPhoto.eee2f322.jpg"
+  },
+  {
+    "revision": "4713ef8c327192a71992742a30f6f4ac",
+    "url": "/artyrkozel/socialnet/static/media/user.4713ef8c.png"
+  }
+]);
